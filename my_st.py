b609aff368e0111c3cddcd889b74fe00a34b4626
danii010 <yoonsj@g.hongik.ac.kr>
2026-05-26 15:16:04 +0900

    Add files via upload

--- my_st.py
+++ my_st.py
@@ -1,7 +1,22 @@
 import streamlit as st
 
 # My Streamlit App
-st.title('제목이없었다')
+st.title('스트림릿 어플 만들기 ㅎ')
 
+'## 이건 부제목'
+'# 첫번째 웹 어플 만들기'
 
-##이건부제목 이거도되나
+'''### Magic 적용
+1. ordered item
+ - 강조: **unordered item**
+ - 기울임: *unordered item*
+1. 냐냐냥
+1. 냐냐냐냥
+1. 냐냐냐냐냥
+'''
+
+'''
+- 나옹이
+    - 나나옹이
+        - 나나나옹이
+'''
